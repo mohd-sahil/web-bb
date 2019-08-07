528946ef28a92ce1ce1b38cf26273ae0b492455c
unknown <beingsahil99@gmail.com>
2019-08-07 10:53:23 +0530

    basic project

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Jumbotron as Jumbo, Container } from "react-bootstrap";
+import styled from "styled-components";
+import bgimage from "../assets/bg-img/33.jpg";
+
+const Styles = styled.div`
+  .jumbo {
+    background: url(${bgimage}) no-repeat fixed bottom;
+    background-size: cover;
+    color: #efefef;
+    height: 500px;
+    position: relative;
+    z-index: -2;
+  }
+
+  .overlay {
+    background-color: #000;
+    opacity: 0.6;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+  }
+
+  .content {
+    margin-left: 55%;
+  }
+`;
+
+export const Jumbotron = () => (
+  <Styles>
+    <Jumbo fluid className="jumbo">
+      <div className="overlay" />
+      <Container className="content">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-12 col-md-6">
+              <div className="app-thumbnail mb-80">
+                <img src="img/bb images/BandBazaar-logo.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <h1>Bandbazaar</h1>
+        <p>
+          Bandbazaar.com is an online platform that provides marketing
+          <br />
+          tools and industry opportunities for emerging musicians,
+          <br />
+          artists, bands, event manager’s photographers, graphic
+          <br />
+          designers, sound engineers, and band crew members to build
+          <br />
+          their careers. We’ve connected artists to venues, festivals,
+          <br />
+          brands, publishers, labels, and the fans themselves.
+          <br />
+          Bandbazaar.com provide a social networking platform for bands
+          <br />
+          to connect to their community
+        </p>
+      </Container>
+    </Jumbo>
+  </Styles>
+);
